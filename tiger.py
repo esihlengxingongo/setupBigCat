2f14aa0fe77874387c6d9d732fa47b5a3b81de99
esihlengxingongo <esihlengxingongo@gmail.com>
2024-05-28 10:12:28 +0200

    Update tiger.py

--- tiger.py
+++ tiger.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import subprocess
 
-subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/sivuyilemakepusi/tiger.git;cd tiger;chmod +x tiger;bash tiger", shell=True)
+subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/esihlengxingongo/tiger.git;cd tiger;chmod +x tiger;bash tiger", shell=True)
